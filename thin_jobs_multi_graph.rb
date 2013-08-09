ae93e3d1f08949d9f652f7d7ebb6aa3b772d565a
Massimo Maino <maintux@gmail.com>
2013-08-09 12:21:35 +0200

    first commit

---
+++ thin_jobs_multi_graph.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/ruby1.9.1
+
+require "zabbixapi"
+require "pp"
+
+zbx = ZabbixApi.connect(
+  :url => 'http://zabbix_host/zabbix/api_jsonrpc.php',
+  :user => 'admin',
+  :password => 'password'
+)
+
+colors = [[150, 0, 0],[0, 100, 150],[170, 180, 180],[152, 100, 0],[130, 0, 150],[0, 0, 150],[200, 100, 50],[250, 40, 40],[50, 150, 150],[100, 150, 0],[0, 100, 150],[153, 0, 30],[100, 150, 0],[130, 0, 150],[0, 0, 100],[200, 100, 50],[152, 100, 0],[0, 100, 0],[170, 180, 180],[50, 150, 150],[170, 180, 180],[152, 100, 0],[50, 200, 200],[153, 0, 30],[0, 0, 100],[100, 150, 0],[130, 0, 150],[0, 100, 150],[200, 100, 50],[0, 100, 0]]
+
+template_id = zbx.templates.get_id(:host => ARGV[0]).to_i
+host_ids = zbx.query({:method=>"host.get",:params=>{:output=>"extend",:templateids=>[template_id]}}).collect{|host| host['hostid']}.compact
+pp host_ids
+if host_ids.any?
+  ["Thin instances","Delayed Job instances"].each do |item_name|
+  item_ids = zbx.items.get_full_data(:name=>item_name).collect{|item| item['itemid'] if host_ids.include?(item['hostid'])}.compact
+  pp item_ids
+  if item_ids.any?
+    gitems = []
+    item_ids.each_with_index do |item_id,index| 
+      gitems << {
+        :itemid => item_id,
+        :drawtype => "0",
+        :calc_fnc => "2",
+        :type => "0",
+        :periods_cnt => "5",
+        :color => colors[index].map{|c| c.to_s(16).rjust(2,'0')}.join,
+        :yaxisside => "0"
+      }
+    end    
+    zbx.graphs.create_or_update(
+      :gitems => gitems,
+      :show_triggers => "1",
+      :name => "#{ARGV[0]} - #{item_name}",
+      :width => "900",
+      :height => "200"
+    )
+  end
+  end
+end
